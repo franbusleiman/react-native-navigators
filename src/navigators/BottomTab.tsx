2860ddb7b8e6dfdfed66ba7e5453208f00fd9066
franbusleiman <franbusleiman@gmail.com>
2023-12-15 15:22:09 -0300

    Icons added

--- src/navigators/BottomTab.tsx
+++ src/navigators/BottomTab.tsx
@@ -1,7 +1,7 @@
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import Icon from 'react-native-vector-icons/Ionicons';
 import { AnOtherTab } from '../screens/AnOtherScreen';
 import { StackNavigator } from './StackNavigator';
+import { Icon } from 'react-native-paper';
 
 
 const Tab = createMaterialBottomTabNavigator();
@@ -12,8 +12,8 @@ export const BottomTab = () => {
             labeled={false}
 
         >
-            <Tab.Screen name="Stack" component={StackNavigator} options={{ tabBarIcon: ({ color }) => <Icon name="file-tray-stacked-outline" size={30} color={color} /> }} />
-            <Tab.Screen name="AnOtherTab" component={AnOtherTab} options={{ tabBarIcon: ({ color }) => <Icon name="diamond-outline" size={30} color={color} /> }} />
+            <Tab.Screen name="Stack" component={StackNavigator} options={{ tabBarIcon: ({ color }) => <Icon source="account-plus" size={30} color={color} /> }} />
+            <Tab.Screen name="AnOtherTab" component={AnOtherTab} options={{ tabBarIcon: ({ color }) => <Icon source="account-hard-hat" size={30} color={color} /> }} />
 
         </Tab.Navigator>
     );
